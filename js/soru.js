22b3848d204e3ceb31d096967d3c94061571bee3
Şevval <sevval_kck98@hotmail.com>
2025-03-15 01:06:22 +0300

    15.03.25

---
+++ js/soru.js
@@ -0,0 +1,12 @@
+function Soru(soruMetni, cevapSecenekleri, dogruCevap) {
+    // const soruMetni = document.getElementById("soruMetni");
+    this.soruMetni = soruMetni ;
+    this.cevapSecenekleri = cevapSecenekleri ;
+    this.dogruCevap = dogruCevap ;
+}
+
+
+Soru.prototype.cevabiKontrolEt = function(cevap) {
+    return cevap === this.dogruCevap;
+}
+
